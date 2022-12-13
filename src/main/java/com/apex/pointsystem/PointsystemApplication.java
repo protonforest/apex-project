6aa0aedc5a455ad14f86551877fd58924c241ee1
Hussein Karaki <protonforest@gmail.com>
2022-12-13 14:16:42 -0500

    initial

---
+++ src/main/java/com/apex/pointsystem/PointsystemApplication.java
@@ -0,0 +1,13 @@
+package com.apex.pointsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PointsystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PointsystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/apex/pointsystem/PointsystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.apex.pointsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PointsystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
